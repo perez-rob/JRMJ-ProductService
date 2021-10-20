222775bf6bd7e56b38f1ab5cc13cbbe9ac1b20fb
bob.w.perez <bob.w.perez@gmail.com>
2021-10-20 18:20:02 -0400

    changing annotations

--- src/main/java/com/jrmj/ProductService/ProductServiceApplication.java
+++ src/main/java/com/jrmj/ProductService/ProductServiceApplication.java
@@ -1,10 +1,13 @@
 package com.jrmj.ProductService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableEurekaClient
 public class ProductServiceApplication {
 
