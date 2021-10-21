5b994897d2f47a39a689b59f2c6a5114cea5d05b
bob.w.perez <bob.w.perez@gmail.com>
2021-10-21 11:33:36 -0400

    change properties to yml

--- src/main/java/com/jrmj/ProductService/ProductServiceApplication.java
+++ src/main/java/com/jrmj/ProductService/ProductServiceApplication.java
@@ -1,15 +1,19 @@
 package com.jrmj.ProductService;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableEurekaClient
+@SpringBootApplication
+@RestController
 public class ProductServiceApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
