188a3b1e17999031809390316aa206ff9b7578f5
bob.w.perez <bob.w.perez@gmail.com>
2021-10-22 04:39:57 -0400

    still trying to register

--- src/main/java/com/jrmj/ProductService/ProductServiceApplication.java
+++ src/main/java/com/jrmj/ProductService/ProductServiceApplication.java
@@ -3,17 +3,11 @@ package com.jrmj.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
+@EnableDiscoveryClient
 public class ProductServiceApplication {
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello world";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
